5df664aec12753116bb947220a6e5d3c91dd9064
guxo2795 <guxo1211@gmail.com>
2025-03-18 11:29:55 +0900

    백준 단계별 문제풀기 : 배열_HashSet

--- src/백준/단계별/단계4_배열/서로다른나머지.java
+++ src/백준/단계별/단계4_배열/서로다른나머지.java
@@ -3,12 +3,13 @@ package 백준.단계별.단계4_배열;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 
-public class HashSet {
+public class 서로다른나머지 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        java.util.HashSet<Integer> set = new java.util.HashSet<>();
+        HashSet<Integer> set = new HashSet<>();
 
         for(int i=0; i<10; i++) {
             set.add(Integer.parseInt(br.readLine())%42);
